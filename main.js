e52baf1915007ae9698b9e99e8a013fa0b08945b
bimahp <me@bimahp.com>
2016-07-26 21:42:46 +0700

    Add secondary filter by CP for sort by name and id

--- main.js
+++ main.js
@@ -609,12 +609,18 @@ function sortAndShowBagPokemon(sortOn, user_id) {
       sortedPokemon.sort(function(a, b){
         if(a.name < b.name) return -1;
         if(a.name > b.name) return 1;
+        if(a.cp > b.cp) return -1;
+        if(a.cp < b.cp) return 1;
         return 0;
       });
       break;
     case 'id':
       sortedPokemon.sort(function(a, b){
-        return a.id - b.id
+        if(a.id < b.id) return -1;
+        if(a.id > b.id) return 1;
+        if(a.cp > b.cp) return -1;
+        if(a.cp < b.cp) return 1;
+        return 0;
       });
       break;
     case 'cp':
