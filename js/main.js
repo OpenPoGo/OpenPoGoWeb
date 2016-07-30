OpenPoGo/OpenPoGoWeb

commit 4cee7e194bf926a70d157e1a0814af07c9bb0619

Show current and max hp with health bar (#109)
* Show current and max hp with health bar

* Update main.js

Fixed string concatenating

* Modified styling healthbar

* Updated hp bar and some minor visual tweaks

js/main.js
@@ -533,6 +533,8 @@ var mapView = {
         pkmIVA = pokemonData.individual_attack || 0,
         pkmIVD = pokemonData.individual_defense || 0,
         pkmIVS = pokemonData.individual_stamina || 0,
+        pkmHP = pokemonData.stamina || 0,
+        pkmMHP = pokemonData.stamina_max || 0,
         pkmIV = ((pkmIVA + pkmIVD + pkmIVS) / 45.0).toFixed(2),
         pkmTime = pokemonData.creation_time_ms || 0;
 
@@ -544,7 +546,10 @@ var mapView = {
         "attack": pkmIVA,
         "defense": pkmIVD,
         "stamina": pkmIVS,
-        "creation_time": pkmTime
+        "health": pkmHP,
+        "max_health": pkmMHP,
+        "creation_time": pkmTime,
+        'candy': self.getCandy(pkmID, user_id)
       });
     }
     switch (sortOn) {
@@ -587,6 +592,13 @@ var mapView = {
           return 0;
         });
         break;
+      case 'candy':
+        sortedPokemon.sort(function(a, b) {
+          if (a.candy > b.candy) return -1;
+          if (a.candy < b.candy) return 1;
+          return 0;
+        });
+        break;
       default:
         sortedPokemon.sort(function(a, b) {
           if (a.cp > b.cp) return -1;
@@ -604,20 +616,18 @@ var mapView = {
         pkmnIVA = sortedPokemon[i].attack,
         pkmnIVD = sortedPokemon[i].defense,
         pkmnIVS = sortedPokemon[i].stamina,
+        pkmnHP = sortedPokemon[i].health,
+        pkmnMHP = sortedPokemon[i].max_health,
         candyNum = self.getCandy(pkmnNum, user_id);
 
       out += '<div class="col s12 m6 l3 center"><img src="image/pokemon/' +
-        pkmnImage +
-        '" class="png_img"><br><b>' +
+        pkmnImage + '" class="png_img"><br><b>' +
         pkmnName +
-        '</b><br>' +
-        pkmnCP +
-        '<br>IV: ' +
-        pkmnIV +
-        '<br>A/D/S:' +
-        pkmnIVA + '/' + pkmnIVD + '/' + pkmnIVS +
-        '<br>Candy: ' +
-        candyNum +
+        '</b><br><div class="progress pkmn-progress pkmn-' + pkmnNum + '"> <div class="determinate pkmn-' + pkmnNum + '" style="width: ' + (pkmnHP / pkmnMHP) * 100 +'%"></div> </div>'+
+        '<b>HP:</b> ' + pkmnHP + ' / ' + pkmnMHP +
+        '<br><b>IV:</b> ' + (pkmnIV >= 0.8 ? '<span style="color: #039be5">' + pkmnIV + '</span>' : pkmnIV) +
+        '<br><b>A/D/S:</b> ' + pkmnIVA + '/' + pkmnIVD + '/' + pkmnIVS +
+        '<br><b>Candy: </b>' + candyNum +
         '</div>';
     }
     // Add number of eggs

css/main.css
@@ -38,7 +38,7 @@ html, body {
 .items {
 	overflow-y: scroll;
 	overflow-x: hidden;
-	height: 80vh; 
+	height: 80vh;
 }
 .item_img {
 	width: 75px;
@@ -67,13 +67,13 @@ html, body {
   top: auto !important;
   right: auto !important;
   bottom: 10%;
-  left:7%;  
+  left:7%;
 }
 #bots-list {
 	max-height: 80vh;
 }
-#bots-list::-webkit-scrollbar { 
-	display: none; 
+#bots-list::-webkit-scrollbar {
+	display: none;
 }
 .collapsible-body {
 	background: #FFF;
@@ -252,4 +252,11 @@ html, body {
 }
 .botbar-13 {
 	background-color: #EDBB99;
+}
+
+.pkmn-progress {
+    width: 80%;
+    height: 5px;
+    margin: 0.3rem auto;
+    max-width: 96px;
 }

commit a6b6a833f6728c05218a575a2fe05d79768ac134

Re-add Pokémon CP
#4cee7e1 accidentally removed Pokémon CP stat listing so we add it back in.

js/main.js
@@ -627,6 +627,7 @@ var mapView = {
         pkmnName +
         '</b><br><div class="progress pkmn-progress pkmn-' + pkmnNum + '"> <div class="determinate pkmn-' + pkmnNum + '" style="width: ' + (pkmnHP / pkmnMHP) * 100 +'%"></div> </div>'+
         '<b>HP:</b> ' + pkmnHP + ' / ' + pkmnMHP +
+        '<br><b>CP:</b>' + pkmnCP +
         '<br><b>IV:</b> ' + (pkmnIV >= 0.8 ? '<span style="color: #039be5">' + pkmnIV + '</span>' : pkmnIV) +
         '<br><b>A/D/S:</b> ' + pkmnIVA + '/' + pkmnIVD + '/' + pkmnIVS +
         '<br><b>Candy: </b>' + candyNum +